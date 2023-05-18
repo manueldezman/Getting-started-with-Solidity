c6214f60de8407026a35ebb57802442f40c9f57c
Abdulganiy Adeleke <adelekeabdulganiy@gmail.com>
2023-05-18 05:26:46 +0100

    Create get_n_set_fuctions.sol

---
+++ get_n_set_fuctions.sol
@@ -0,0 +1,47 @@
+//SPDX-Llicense-Identifier: MIT
+pragma solidity 0.8.7;
+
+contract var_types {
+
+    //Variable definitions
+    int positive_value;
+    uint negative_value;
+    bool booln_value;
+    address my_address;
+
+// set the the value for positive integer 
+function set_pos(int positive_value) public {
+    positive_value = 5;
+}
+// return the value of the posiitve integer
+function get_pos() public view {
+    return positive_value;
+}
+
+// set the the value for negative integer 
+function set_neg(uint negative_value) public {
+    negative_value = -5;
+}
+// return the value of the negative integer
+function get_neg() public view {
+    return negative_value;
+}
+
+// set a boolean value 
+function set_bool(bool booln_value) public {
+    booln_value = true;
+}
+// return the boolean value
+function get_bool() public view {
+    return booln_value;
+}
+
+// set the the value for my address
+function set_address(address my_address) public {
+    my_address = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+}
+// return the value of my address
+function get_address() public view {
+    return my_address;
+}
+}
