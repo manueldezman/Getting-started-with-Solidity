c4691c834615cf0cde37ee8467da777b6e3d707a
Abdulganiy Adeleke <adelekeabdulganiy@gmail.com>
2023-05-19 22:19:25 +0100

    Update get_n_set_fuctions.sol

--- get_n_set_fuctions.sol
+++ get_n_set_fuctions.sol
@@ -1,4 +1,4 @@
-//SPDX-Llicense-Identifier: MIT
+//SPDX-License-Identifier: MIT
 pragma solidity 0.8.7;
 
 contract var_types {
@@ -10,38 +10,38 @@ contract var_types {
     address my_address;
 
 // set the the value for positive integer 
-function set_pos(uint positive_value) public {
-    positive_value = 5;
+function set_pos(uint _positive_value) public {
+    positive_value = _positive_value;
 }
 // return the value of the posiitve integer
-function get_pos() public view {
+function get_pos() public view returns(uint){
     return positive_value;
 }
 
 // set the the value for negative integer 
-function set_neg(int negative_value) public {
-    negative_value = -5;
+function set_neg(int _negative_value) public {
+    negative_value = _negative_value;
 }
 // return the value of the negative integer
-function get_neg() public view {
+function get_neg() public view returns(int){
     return negative_value;
 }
 
 // set a boolean value 
-function set_bool(bool booln_value) public {
-    booln_value = true;
+function set_bool(bool _booln_value) public {
+    booln_value = _booln_value;
 }
 // return the boolean value
-function get_bool() public view {
+function get_bool() public view returns(bool){
     return booln_value;
 }
 
 // set the the value for my address
-function set_address(address my_address) public {
-    my_address = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+function set_address(address _my_address) public {
+    my_address = _my_address;
 }
 // return the value of my address
-function get_address() public view {
+function get_address() public view returns(address){
     return my_address;
 }
 }
